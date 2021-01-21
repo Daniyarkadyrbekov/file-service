2bd7734d87ca968d6d65cc6d5fad6af79e3b5b2c
d.kadyrbekov <d.kadyrbekov@dlg.im>
2021-01-21 07:10:41 +0300

    diryctory replication

--- client/main.go
+++ client/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Creates a new file upload http request with optional extra params
-func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
+func newfileUploadRequest(uri string, params map[string]string, paramName, basePath, path string) (*http.Request, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -21,7 +21,12 @@ func newfileUploadRequest(uri string, params map[string]string, paramName, path 
 
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	fmt.Printf("path = %s\n", path)
+	//fileName, err := filepath.Rel(basePath, path)
+	if err != nil {
+		return nil, err
+	}
+	part, err := writer.CreateFormFile(paramName, path)
 	if err != nil {
 		return nil, err
 	}
@@ -41,30 +46,75 @@ func newfileUploadRequest(uri string, params map[string]string, paramName, path 
 }
 
 func main() {
-	path, _ := os.Getwd()
-	path += "/example1"
+	//path, _ := os.Getwd()
+	//path += "/example2"
+	//extraParams := map[string]string{
+	//	"title":       "My Document",
+	//	"author":      "Matt Aimonetti",
+	//	"description": "A document with all the Go programming language secrets",
+	//}
+	//request, err := newfileUploadRequest("http://localhost:8080/upload", extraParams, "myFile", path)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//client := &http.Client{}
+	//resp, err := client.Do(request)
+	//if err != nil {
+	//	log.Fatal(err)
+	//} else {
+	//	body := &bytes.Buffer{}
+	//	_, err := body.ReadFrom(resp.Body)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	resp.Body.Close()
+	//	fmt.Println(resp.StatusCode)
+	//	fmt.Println(resp.Header)
+	//	fmt.Println(body)
+	//}
+
 	extraParams := map[string]string{
 		"title":       "My Document",
 		"author":      "Matt Aimonetti",
 		"description": "A document with all the Go programming language secrets",
 	}
-	request, err := newfileUploadRequest("http://localhost:8080/upload", extraParams, "myFile", path)
+	client := &http.Client{}
+	workDir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	client := &http.Client{}
-	resp, err := client.Do(request)
+
+	err = filepath.Walk("./logs",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+
+			request, err := newfileUploadRequest("http://localhost:8080/upload", extraParams, "myFile", workDir, path)
+			if err != nil {
+				return err
+			}
+			resp, err := client.Do(request)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				body := &bytes.Buffer{}
+				_, err := body.ReadFrom(resp.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+				resp.Body.Close()
+				fmt.Println(resp.StatusCode)
+				fmt.Println(resp.Header)
+				fmt.Println(body)
+			}
+
+			return nil
+		})
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		body := &bytes.Buffer{}
-		_, err := body.ReadFrom(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		resp.Body.Close()
-		fmt.Println(resp.StatusCode)
-		fmt.Println(resp.Header)
-		fmt.Println(body)
+		log.Println(err)
 	}
 }
--- server/main.go
+++ server/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
@@ -26,24 +28,30 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer tempFile.Close()
-
 	// read all of the contents of our uploaded file into a
 	// byte array
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
+	tempFile, err := create(handler.Filename)
 	// write this byte array to our temporary file
-	tempFile.Write(fileBytes)
+	if _, err := tempFile.Write(fileBytes); err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	// return that we have successfully uploaded our file!
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func create(p string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(p), 0770); err != nil {
+		return nil, err
+	}
+	return os.Create(p)
 }
 
 func main() {
